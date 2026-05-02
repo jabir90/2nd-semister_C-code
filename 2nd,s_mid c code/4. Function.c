98f48a95fe248d9e215b595079af09e3f2f60183
Jabir Ahmad <universitylifejabir@gmail.com>
2026-05-03 01:05:54 +0600

    Add files via upload

---
+++ 2nd,s_mid c code/4. Function.c
@@ -0,0 +1,19 @@
+// Basic function creasion.
+#include <stdio.h>
+
+void area(float a, float b);
+
+int main()
+{
+    int a, b;
+    printf("Enter lanth and width value: ");
+    scanf("%d %d", &a, &b);
+    area(a, b);
+    return 0;
+}
+void area(float a, float b)
+{
+    int sum;
+    sum = a * b;
+    printf("The area of a ractangle is: %d", sum);
+}
